ca9bfb18188d58034a7ac06fdb38fc4b7969f1a4
Aryan Shriva <89904554+AryanShriva@users.noreply.github.com>
2025-09-22 19:24:03 +0530

    Time: 0 ms (100%), Space: 45.4 MB (56.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,30 @@
 //using sum method (n*(n+1))/2
+// class Solution {
+//         public int missingNumber(int[] nums) {
+//                 int n = nums.length;
+//                 int expectedSum = n * (n + 1) / 2;
+//                 int actualSum = 0;
+
+//                 for (int num : nums) {
+//                     actualSum += num;
+//                 }
+
+//                 return expectedSum - actualSum;
+//         }
+// }
+
+
+//using xor
 class Solution {
-        public int missingNumber(int[] nums) {
-                int n = nums.length;
-                int expectedSum = n * (n + 1) / 2;
-                int actualSum = 0;
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i ^ nums[i];
+        }
+        return xor;
+    }
+}
 
-                for (int num : nums) {
-                    actualSum += num;
-                }
-
-                return expectedSum - actualSum;
-            }
-       }
 
 
 
